0be5c9a90870857873ba2d0ff52dde0f45f7bf45
Daniel Côté <dccote@cervo.ulaval.ca>
2025-03-13 02:19:42 -0300

    Created a Solver delegate
    We can use CPU or GPU with a delegate
    upscale() with quadratic interpolation is the best option with factor=8

--- Semaine-08 Laplace/laplace.py
+++ Semaine-08 Laplace/laplace.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import time
 from scipy.ndimage import zoom
 from enum import Enum
+import math
 
 try:
 	import pyopencl as cl
@@ -20,55 +21,238 @@ all    = slice(None, None)
 
 class ScalarField:
 	def __init__(self, shape):
-		self.field = np.zeros(shape=shape, dtype=np.float32)
+		self.values = np.zeros(shape=shape, dtype=np.float32)
 		self.conditions = []
+		self.solver = LaplacianSolver()
+
+	def reset(self, shape=None):
+		if shape is None:
+			shape = self.values.shape
+
+		self.values = np.zeros(shape=shape, dtype=np.float32)
+
+	def upscale(self, factor=2, order=2):
+		self.values = zoom(self.values, factor, order=order)
 
 	def add_boundary_condition(self, index_or_slices, value_or_values):
 		self.conditions.append((index_or_slices, value_or_values))
 
 	def apply_conditions(self):
 		for index_or_slices, value in self.conditions:
-			self.field[*index_or_slices] = value
+			self.values[*index_or_slices] = value
 
 	def solve_laplace_by_relaxation(self, tolerance=1e-7):
-		error = None
-
-		self.apply_conditions()
-
-		while error is None or error > tolerance:
-			before_iteration = self.field.copy()
-			if self.field.ndim == 1:
-				self.field[center] = (self.field[left] + self.field[right])/2
-			elif self.field.ndim == 2:
-				self.field[center, center] = (self.field[left,center] + self.field[right,center] +
-											  self.field[center,left] + self.field[center,right])/4
-			elif self.field.ndim == 3:
-				self.field[center, center, center] = (self.field[left, center, center] + self.field[center, left,center] +
-												  self.field[center, center, left] + self.field[right,center,center] + 
-												  self.field[center, right,center] + self.field[center, center, right])/6
-			else:
-				raise ValueError('Unable to manage dimension > 3')
-
-			self.apply_conditions()
-			error = np.std(self.field - before_iteration)
-
-		return self.field
+		return self.solver.solve_by_relaxation(self, tolerance=tolerance)
 
 	def show(self, slices=None, title=None, block=False):
 		plt.clf()
 		plt.title(title)
-		if self.field.ndim == 1:
-			plt.plot(self.field)
+		if self.values.ndim == 1:
+			plt.plot(self.values)
 		else:
 			if slices is not None:
-				plt.imshow(self.field[*slices])
+				plt.imshow(self.values[*slices])
 			else:
-				plt.imshow(self.field)
+				plt.imshow(self.values)
 
 		if block:
 			plt.show()
 		else:
 			plt.pause(0.5)
+
+class LaplacianSolver:
+	def solve_by_relaxation(self, field, tolerance):
+		if field.values.ndim == 1:
+			return self.solve1D_by_relaxation(field, tolerance)
+		elif field.values.ndim == 2:
+			return self.solve2D_by_relaxation(field, tolerance)
+		elif field.values.ndim == 3:
+			return self.solve3D_by_relaxation(field, tolerance)
+		else:
+			raise ValueError('Unable to manage dimension > 3')
+
+	def solve1D_by_relaxation(self, field, tolerance):
+		error = None
+		field.apply_conditions()
+		i = 0
+		while error is None or error > tolerance:
+			before_iteration = field.values.copy()
+			field.values[center] = (field.values[left] + field.values[right])/2
+			field.apply_conditions()
+			error = np.std(field.values - before_iteration)
+			error = np.std(field.values - before_iteration)
+			i += 1
+
+		return i
+
+	def solve2D_by_relaxation(self, field, tolerance):
+		error = None
+
+		field.apply_conditions()
+		i = 0
+		while error is None or error > tolerance:
+			if i % 100 == 0:
+				before_iteration = field.values.copy()
+
+			field.values[center, center] = (field.values[left,center] + field.values[right,center] +
+									 field.values[center,left] + field.values[center,right])/4
+			field.apply_conditions()
+			if i % 100 == 0:
+				error = np.std(field.values - before_iteration)
+			i += 1
+
+		return i
+
+	def solve3D_by_relaxation(self, field, tolerance):
+		error = None
+
+		field.apply_conditions()
+		i = 0
+		while error is None or error > tolerance:
+			if i % 100 == 0:
+				before_iteration = field.values.copy()
+			field.values[center, center, center] = (field.values[left, center, center] + field.values[center, left,center] +
+											 field.values[center, center, left] + field.values[right,center,center] + 
+											 field.values[center, right,center] + field.values[center, center, right])/6
+			field.apply_conditions()
+			if i % 100 == 0:
+				error = np.std(field.values - before_iteration)
+			i += 1
+
+		return i
+
+class LaplacianSolverGPU(LaplacianSolver):
+	def __init__(self):
+		super().__init__()
+		self.platform = cl.get_platforms()[0]  # Select first platform
+		self.device = self.platform.get_devices()[0]  # Select first device (GPU or CPU)
+		self.context = cl.Context([self.device])  # Create OpenCL context
+		self.queue = cl.CommandQueue(self.context)  # Create command queue
+		self.program = cl.Program(self.context, self.kernel_code).build()
+
+	def solve2D_by_relaxation(self, field, tolerance):
+		field.apply_conditions()
+
+		# Create OpenCL buffers: use cl.Array to benefit from operator overloading
+		d_input = cl_array.to_device(self.queue, field.values)  # Copy data to GPU
+		d_output = cl_array.empty_like(d_input)  # Create an empty GPU array
+
+		h, w = field.values.shape
+		global_size = field.values.shape
+		size = math.prod(global_size)
+
+		error = None
+		i = 0
+		while error is None or error > tolerance:
+			# The calculation is sent to d_output, which I then use as the input for another iteration
+			# This way, d_input becomes the output and I do not have to create an array each time.  This is very efficient.
+
+			self.program.laplace2D(self.queue, global_size, None, d_input.data, d_output.data, np.int32(w))
+			self.program.laplace2D(self.queue, global_size, None, d_output.data, d_input.data, np.int32(w))
+
+			if i % 100 == 0:
+				error = self.variance(d_output - d_input)
+			i += 1
+
+		field.values = d_input.get()
+		return i
+
+	def solve3D_by_relaxation(self, field, tolerance):
+		field.apply_conditions()
+
+		# Create OpenCL buffers
+		d_input = cl_array.to_device(self.queue, field.values)  # Copy data to GPU
+		d_output = cl_array.empty_like(d_input)  # Create an empty GPU array
+
+		# Set up the execution parameters
+		d, h, w = field.values.shape
+		global_size = field.values.shape
+		size = math.prod(global_size)
+
+		error = None
+		i = 0
+		while error is None or error > tolerance:
+			self.program.laplace3D(self.queue, global_size, None, d_input.data, d_output.data, np.int32(w), np.int32(h), np.int32(d))
+			# The calculation is sent to d_output, which I then use as the input for another iteration
+			# This way, d_input becomes the output and I do not have to create an array each time.  This is very efficient.
+			self.program.laplace3D(self.queue, global_size, None, d_output.data, d_input.data, np.int32(w), np.int32(h), np.int32(d))
+
+			if i % 100 == 0:
+				error = self.variance(d_output - d_input)
+			i += 1
+
+		# Retrieve results
+		field.values = d_output.get()
+		return i
+
+	def variance(self, d_diff):
+		size = math.prod(d_diff.shape)
+
+		mean_val = cl_array.sum(d_diff).get() / size  # Mean (transfers single float)
+		d_diff_sq = (d_diff - mean_val) ** 2  # Element-wise (x - mean)^2
+		variance_val = cl_array.sum(d_diff_sq).get() / size  # Variance (transfers single float)
+		return np.sqrt(variance_val)  # Standard deviation (final sqrt)
+
+	@property
+	def kernel_code(self):
+		kernel_code = """
+
+		__kernel void laplace2D(__global float* input, __global float* output, int width) {
+		    int x = get_global_id(0);
+		    int y = get_global_id(1);
+		    int index = y * width + x;
+
+		    if (x == 0 || y == 0 || x == width-1 || y == width-1) {
+		    	output[index] = input[index]; // Boundary is fixed
+		    } else {
+				output[index] = (input[index-1] + input[index+1] + input[index-width] + input[index+width])/4;
+			}
+		}
+
+		__kernel void laplace3D(__global float* input, __global float* output, int width, int height, int depth) {
+		    int x = get_global_id(0);
+		    int y = get_global_id(1);
+		    int z = get_global_id(2);
+
+		    int index = z * (width * height) + y * width + x;
+
+		    if (x == 0 || y == 0 || z == 0 || x == width-1 || y == height-1 || z == depth-1) {
+		    	output[index] = input[index];
+		    } else {
+				output[index] = (input[index-1] + input[index+1] + input[index-width] + input[index+width] + input[index-width*height] + input[index+width*height])/6;
+			}
+		}
+
+
+		__kernel void zoom2D_nearest_neighbour(__global float* input, __global float* output, int width, int height) {
+		    int x = get_global_id(0);
+		    int y = get_global_id(1);
+
+		    int index_src = y * width + x;
+
+		    int index_dest1 = (2*y) * (2*width) + 2*x;
+		    int index_dest2 = (2*y) * (2*width) + 2*x + 1;
+		    int index_dest3 = (2*y) * (2*width) + 2*x + 2*width;
+		    int index_dest4 = (2*y) * (2*width) + 2*x + 2*width + 1;
+
+	    	output[index_dest1] = input[index_src];
+	    	output[index_dest2] = input[index_src];
+	    	output[index_dest3] = input[index_src];
+	    	output[index_dest4] = input[index_src];
+		}
+
+		__kernel void copy(__global float* input, __global float* output, int width) {
+		    int x = get_global_id(0);
+		    int y = get_global_id(1);
+
+		    int index = y * width + x;
+
+	    	output[index] = input[index];
+		}
+
+		"""
+		return kernel_code
+
 
 class PotentialTestCase(unittest.TestCase):
 	def test_init(self):
@@ -99,6 +283,96 @@ class PotentialTestCase(unittest.TestCase):
 
 		pot.show(title=self._testMethodName)
 
+	def test_solve2D_then_upscale(self):
+		pot = ScalarField(shape=(8,8))
+		pot.solver = LaplacianSolverGPU()
+		pot.add_boundary_condition( (all, 0), 0)
+		pot.add_boundary_condition( (all, -1), 0)
+		pot.add_boundary_condition( (0, all), 10)
+		pot.add_boundary_condition( (-1, all), 5)
+
+		pot.apply_conditions()
+		start_time = time.time()		
+		pot.solve_laplace_by_relaxation(tolerance=1e-6)
+		pot.upscale(factor=8, order=2)
+		pot.show(title=f"Upscaled solution", block=True)
+
+		pot.solve_laplace_by_relaxation(tolerance=1e-6)
+		pot.show(title=f"Actual solution", block=True)
+
+	def test_solve2D_with_upscale(self):
+		pot = ScalarField(shape=(8,8))
+		pot.solver = LaplacianSolverGPU()
+		pot.add_boundary_condition( (all, 0), 0)
+		pot.add_boundary_condition( (all, -1), 0)
+		pot.add_boundary_condition( (0, all), 10)
+		pot.add_boundary_condition( (-1, all), 5)
+
+		pot.apply_conditions()
+
+		start_time = time.time()		
+		pot.solve_laplace_by_relaxation(tolerance=1e-6)
+		pot.upscale(factor=16, order=0)
+		pot.apply_conditions()
+		it = pot.solve_laplace_by_relaxation(tolerance=1e-6)
+		print(f"[{it}] With NN refinement: {time.time()-start_time:.2f}")
+
+		pot.reset(shape=(8,8))
+		start_time = time.time()		
+		pot.solve_laplace_by_relaxation(tolerance=1e-6)
+		pot.upscale(factor=16, order=1)
+		pot.apply_conditions()
+		it = pot.solve_laplace_by_relaxation(tolerance=1e-6)
+		print(f"[{it}] With lin refinement: {time.time()-start_time:.2f}")
+
+		pot.reset(shape=(8,8))
+		start_time = time.time()		
+		pot.solve_laplace_by_relaxation(tolerance=1e-6)
+		pot.upscale(factor=16, order=2)
+		pot.apply_conditions()
+		it = pot.solve_laplace_by_relaxation(tolerance=1e-6)
+		print(f"[{it}] With quad refinement: {time.time()-start_time:.2f}")
+
+		pot.reset(shape=(8,8))
+		start_time = time.time()		
+		pot.solve_laplace_by_relaxation(tolerance=1e-6)
+		pot.upscale(factor=8, order=2)
+		pot.apply_conditions()
+		it = pot.solve_laplace_by_relaxation(tolerance=1e-6)
+		pot.upscale(factor=8, order=2)
+		pot.apply_conditions()
+		it = pot.solve_laplace_by_relaxation(tolerance=1e-6)
+		print(f"[{it}] With quad refinement: {time.time()-start_time:.2f}")
+		pot.show(title=f"{pot.values.shape}", block=True)
+
+		# pot.reset()
+		# pot.apply_conditions()
+		# start_time = time.time()		
+		# it = pot.solve_laplace_by_relaxation(tolerance=1e-6)
+		# pot.show(title=f"{self._testMethodName} {pot.values.shape}")
+		# print(f"[{it}] Without refinement: {time.time()-start_time:.2f}")
+
+	def test_solve2D_CPU_vs_GPU(self):
+		pot = ScalarField(shape=(32,32))
+		pot.solver = LaplacianSolver()
+
+		pot.add_boundary_condition( (0, all), 10)
+		pot.add_boundary_condition( (-1, all), 5)
+		pot.apply_conditions()
+		pot.solver = LaplacianSolverGPU()
+
+		pot.solve_laplace_by_relaxation()
+		field_gpu = pot.values.copy()
+		pot.show(title=f"{self._testMethodName} [GPU]")
+		
+		pot.values = np.zeros(shape=pot.values.shape, dtype=np.float32)
+		pot.solver = LaplacianSolver()
+
+		pot.solve_laplace_by_relaxation()
+		field_cpu = pot.values.copy()
+		self.assertTrue( (field_cpu-field_gpu).all() == 0)
+		pot.show(title=f"{self._testMethodName} [CPU]")
+
 	def test_solve2D_function_condition(self):
 		pot = ScalarField(shape=(32,32))
 
@@ -125,6 +399,7 @@ class PotentialTestCase(unittest.TestCase):
 	def test_conditions3D(self):
 		pot = ScalarField(shape=(32,32,32))
 		pot.add_boundary_condition( (all, 0, 0), 10)
+		pot.apply_conditions()
 
 	def test_solve1D(self):
 		pot = ScalarField(shape=(32))
@@ -138,18 +413,18 @@ class PotentialTestCase(unittest.TestCase):
 		pot.show(title=self._testMethodName)
 
 	def test_solve3D(self):
-		pot = ScalarField(shape=(32,32,32))
+		pot = ScalarField(shape=(64,64,64))
 
-		pot.add_boundary_condition( ( -1, all, all), 0)
+		pot.add_boundary_condition( ( -1, all, all), 10)
 		pot.add_boundary_condition( (all,   0, all), 0)
 		pot.add_boundary_condition( (all,  -1, all), 0)
 		pot.add_boundary_condition( (all, all,   0), 0)
 		pot.add_boundary_condition( (all, all,  -1), 0)
 		pot.add_boundary_condition( ( 0 , all, all), 10)
 		pot.apply_conditions()
-		pot.solve_laplace_by_relaxation(tolerance=1e-8)
+		pot.solve_laplace_by_relaxation(tolerance=1e-7)
 
-		pot.show(slices=(all, 15, all), title=self._testMethodName, block=True)
+		pot.show(slices=(all, 31, all), title=self._testMethodName)
 
 
 if __name__ == "__main__":
